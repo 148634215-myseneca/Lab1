826a7e5473484537606d80f4c71a564510538a86
Darshit Patel <130853558+148634215-myseneca@users.noreply.github.com>
2023-06-13 23:02:06 -0400

    Update Lab1.py

--- Lab1.py
+++ Lab1.py
@@ -1,19 +1,20 @@
+
 def helloWorld():
     print('Hello World')
     
 helloWorld()
 
+
 import datetime
 
 def calculate_age():
-    try:
-        birth_year = int(input("Enter your birth year: "))
-        current_year = 2023  # You can update this with the current year
-        age = current_year - birth_year
-        print("Your age is:", age)
-    except ValueError:
-        print("Please enter a valid integer for the birth year.")
+    current_year = datetime.datetime.now().year
+    birth_year = input("Enter your birth year: ")
+    birth_year = int(birth_year)
+    age = current_year - birth_year
+    print("Your age is:", age)
 
 
-if __name__ == "__main__":
-    calculate_age()
+# Call the function
+calculate_age()
+
